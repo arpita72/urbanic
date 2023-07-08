8ca8c35a72c778460f25936091f7e8088890c14d
arpita72 <shuklaarpita476@gmail.com>
2023-07-08 11:32:13 -0700

    urbanic

---
+++ src/main/java/urbanic/App.java
@@ -0,0 +1,13 @@
+package urbanic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
